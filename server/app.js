eaf5a4b110151d00be81d8485ab18ed521d786db
Dmitry Rakov <rakov.di@gmail.com>
2020-03-15 23:32:15 +0300

    [+] Добавляет установку реального автора и сообщени коммита при постановке его в очередь на сборку

--- server/app.js
+++ server/app.js
@@ -9,33 +9,97 @@ const { spawn } = require('child_process');
 
 const app = express();
 
-let authorizationToken = getToken();
+let isRepoExist = false;
+const localRepoName = 'local_repo';
+let mainBranch;
 
-function getToken() {
+getToken = () => {
   return fs.readFileSync(path.resolve(__dirname, '../../_access/token.txt'), 'utf8', (err, token) => {
     return token;
   });
-}
+};
+
+
+// const log = spawn(`git show -s --format='%s===%an' 2cd1deef703e8071946608f140ee1091fe89da5d`, {shell: true});
+//
+// log.stdout.on('data', (data) => {
+//   console.log(data.toString().trim().split("==="));
+//   // console.log(`stdout: ${data}`);
+// });
+//
+// log.stderr.on('data', (data) => {
+//   // next(error);
+//   console.error(`stderr: ${data}`);
+// });
+
+//
+//
+//
+// gitClone = () => {
+//   fs.access(path.resolve(__dirname, localRepoName), (err) => {
+//     if (err && err.code === 'ENOENT') {
+//     }
+//     else {
+//     }
+//
+//   const gitClone = spawn('git', ['clone', req.body.repoName, './local_repo']);
+//   const rmDir = spawn('rm', ['-rf', localRepoName]);
+//
+//   gitClone.stdout.on('data', (data) => {
+//     isRepoExist = true;
+//     console.log(`stdout: ${data}`);
+//   });
+//
+//   gitClone.stderr.on('data', (data) => {
+//     next(error);
+//     console.error(`stderr: ${data}`);
+//   });
+//
+//   });
+// };
+//
+// gitFetch = (branchName) => {
+//   const changeDir = spawn('cd', ['local_repo']);
+//   const gitCheckout = spawn('git', ['checkout', branchName]);
+//   const gitFetch = spawn('git', ['pull']);
+//
+//   changeDir.stdout.pipe(gitCheckout.stdin);
+//   gitCheckout.stdout.pipe(gitFetch.stdin);
+//
+//   gitFetch.stdout.on('data', (data) => {
+//     console.log(`stdout: ${data}`);
+//   });
+//
+//   gitFetch.stderr.on('data', (data) => {
+//     next(error);
+//     console.error(`stderr: ${data}`);
+//   });
+// };
 
 const api = axios.create({
   baseURL: 'https://hw.shri.yandex/api/',
   timeout: 10000,
   headers: {
-    Authorization: "Bearer " + authorizationToken
+    Authorization: "Bearer " + getToken()
   },
   httpsAgent: new https.Agent({
     rejectUnauthorized: false
   })
 });
 
-// Функции промежуточной обработки (middleware)
 
+
+
+
+// Функции промежуточной обработки (middleware)
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static(path.resolve(__dirname, '../build')));
 app.use(function(err, req, res, next) {
   console.error(err);
 });
+
+// Получение сохраненных настроек репозитория
 
 app.get('/api/settings', (req, res, next) => {
   api.get('/conf')
@@ -47,13 +111,9 @@ app.get('/api/settings', (req, res, next) => {
     });
 });
 
-// Проверялось с get
+// Сохранение (Добавление или обновление) настроек репозитория
+// При это происходит клонирование репозитория на локальную машину
 app.post('/api/settings', (req, res, next) => {
-  console.log('Содержимое' + req.body);
-  console.log('repoName' + req.body.repoName);
-  console.log('repoName' + req.body.buildCommand);
-  console.log('repoName' + req.body.mainBranch);
-  console.log('repoName' + req.body.period);
   api.post('/conf', {
     "repoName": req.body.repoName,
     "buildCommand": req.body.buildCommand,
@@ -61,20 +121,7 @@ app.post('/api/settings', (req, res, next) => {
     "period": +req.body.period
   })
     .then((response) => {
-      const gitClone = spawn('git', ['clone', req.body.repoName, './local_repo']);
-
-      gitClone.stdout.on('data', (data) => {
-        next(error);
-        console.log(`stdout: ${data}`);
-      });
-
-      gitClone.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
-      });
-//
-// ls.on('close', (code) => {
-//   console.log(`child process exited with code ${code}`);
-// });
+      // gitClone();
       res.json('Settings have been updated');
     })
     .catch((error) => {
@@ -82,9 +129,8 @@ app.post('/api/settings', (req, res, next) => {
     });
 });
 
+// Получения списка сборок
 app.get('/api/builds', (req, res, next) => {
-  console.log(req.params.buildId + '1');
-
   api.get('/build/list')
     .then((response) => {
       res.json(response.data);
@@ -94,25 +140,36 @@ app.get('/api/builds', (req, res, next) => {
     });
 });
 
-// Проверялось с get
-app.post('/api/builds1/:commitHash', (req, res, next) => {
-  console.log('request' + req.params.commitHash);
-  api.post('/build/request', {
-      "commitMessage": "[+] Тест",
-      "commitHash": req.params.commitHash,
-      "branchName": "nodejs",
-      "authorName": "Dmitry Rakov"
-    })
-    .then((response) => {
-      res.json(response.data);
-    })
-    .catch((error) => {
-      next(error);
-    });
+// Добавление сборки в очередь для конкретного коммита
+// По полному хэшу коммита определяется полное сообщение, автор. Ветка берется по умолчанию
+app.post('/api/builds/:commitHash', (req, res, next) => {
+  const log = spawn(`git show -s --format='%s===%an' ${req.params.commitHash}`, {shell: true});
+
+  log.stdout.on('data', (data) => {
+    const [message, author] = data.toString().trim().split("===");
+
+    api.post('/build/request', {
+        "commitMessage": message,
+        "commitHash": req.params.commitHash,
+        "branchName": "master",
+        "authorName": author
+      })
+      .then(() => {
+        res.json({message: message, author: author});
+      })
+      .catch((error) => {
+        next(error);
+      });
+  });
+
+  log.stderr.on('data', (data) => {
+    next(error);
+    console.error(`stderr: ${data}`);
+  });
 });
 
+// Получение информации о конкретной сборке
 app.get('/api/builds/:buildId', (req, res, next) => {
-  console.log(req.params.buildId + '2');
   api.get('/build/details?buildId=' + req.params.buildId)
     .then((response) => {
       res.json(response.data);
@@ -122,10 +179,23 @@ app.get('/api/builds/:buildId', (req, res, next) => {
     });
 });
 
+// Получение логов конкретной сборки
 app.get('/api/builds/:buildId/logs', (req, res, next) => {
-  console.log(req.params.buildId + '3');
   api.get('/build/log?buildId=' + req.params.buildId)
     .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+// Тестовая ручка для обновления локального репозитория (подтягивание последних изменений)
+app.get('/api/test', (req, res, next) => {
+  api.get('/conf')
+    .then((response) => {
+      console.log(response.data);
+      // gitFetch(response.data.mainBranch);
       res.send(response.data);
     })
     .catch((error) => {
