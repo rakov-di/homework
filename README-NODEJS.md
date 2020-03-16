900c24bdd284635238f18cd552e684d0863a98e3
Dmitry Rakov <rakov.di@gmail.com>
2020-03-16 03:03:22 +0300

    [+] Добавляет клонирование репы при сохранении настроек и стягивание последних изменений при запросе к специальной ручке

--- README-NODEJS.md
+++ README-NODEJS.md
@@ -12,13 +12,23 @@
 
 Версия Node JS - 11.6.0
 
-Реализовал простое API c командами
+Реализовал отдачу статических страниц:
+ - http://localhost:3000/start-screen.html
+ - http://localhost:3000/settings.html
+ - http://localhost:3000/build-details.html
+ - http://localhost:3000/build-history.html
+ 
+И простое API c командами:
 - GET /api/settings — получение сохраненных настроек
 - POST /api/settings - cохранение настроек
 - GET  /api/builds - получение списка сборок
 - POST /api/builds/:commitHash - добавление сборки в очередь
 - GET  /api/builds/:buildId - получение информации о конкретной сборке
 - GET  /api/builds/:buildId/logs - получение логов билда (сплошной текст)
+
+При `POST /api/settings` кроме проксирования происходит клонирование репозиторя на локальную машину в папку `local_repo`. При повторном запросе репозиторий клонируется заново.
+
+Дополнительн реализована ручка `GET /api/test` для стягивания последних изменений для текущего репозитория.
 
 ## Начало работы <a name = "getting_started"></a>
 
@@ -29,5 +39,7 @@
 ```CLI
 node app.js
 ```
+- Доступ к серверу реализован через `http://localhost:3000`
+
 
 Вернуться на [главную](README.md)
--- server/app.js
+++ server/app.js
@@ -4,55 +4,63 @@ const path = require('path');
 const https = require('https');
 const express = require('express');
 const axios = require('axios');
-
-
 const { spawn } = require('child_process');
 
 const app = express();
 
-let isRepoExist = false;
 const localRepoName = 'local_repo';
 let mainBranch;
 
+cloneRepo = (repoName) => {
+  return new Promise((resolve, reject) => {
+    fs.access(path.resolve(__dirname, localRepoName), (err) => {
+      console.log(2);
+      if (err && err.code === 'ENOENT') {
+        console.log("There is no such folder. You can do git clone");
+        const gitClone = spawn(`git clone ${repoName} local_repo`, {shell: true});
+        gitClone.stdout.on('data', (data) => {
+          console.log(3 + 'a');
+          console.log(`stdout: ${data}`);
+          resolve();
+        });
 
-// const log = spawn(`git show -s --format='%s===%an' 2cd1deef703e8071946608f140ee1091fe89da5d`, {shell: true});
-//
-// log.stdout.on('data', (data) => {
-//   console.log(data.toString().trim().split("==="));
-//   // console.log(`stdout: ${data}`);
-// });
-//
-// log.stderr.on('data', (data) => {
-//   // next(error);
-//   console.error(`stderr: ${data}`);
-// });
+        gitClone.stderr.on('data', (data) => {
+          console.log(3 + ' a error');
+          // next(error);
+          console.error(`stderr: ${data}`);
+          resolve();
+        });
 
-//
-//
-//
-// gitClone = () => {
-//   fs.access(path.resolve(__dirname, localRepoName), (err) => {
-//     if (err && err.code === 'ENOENT') {
-//     }
-//     else {
-//     }
-//
-//   const gitClone = spawn('git', ['clone', req.body.repoName, './local_repo']);
-//   const rmDir = spawn('rm', ['-rf', localRepoName]);
-//
-//   gitClone.stdout.on('data', (data) => {
-//     isRepoExist = true;
-//     console.log(`stdout: ${data}`);
-//   });
-//
-//   gitClone.stderr.on('data', (data) => {
-//     next(error);
-//     console.error(`stderr: ${data}`);
-//   });
-//
-//   });
-// };
-//
+        gitClone.on('close', (data) => {
+          resolve();
+        });
+      }
+      else {
+        console.log('Please, clear folder before git clone');
+        console.log(`rm -rf ${localRepoName} && git clone ${repoName} local_repo`);
+        const gitRmClone = spawn(`rm -rf ${localRepoName} && git clone ${repoName} local_repo`, {shell: true});
+        gitRmClone.stdout.on('data', (data) => {
+          console.log(3 + 'b');
+          console.log(`stdout: ${data}`);
+          resolve();
+        });
+
+        gitRmClone.stderr.on('data', (data) => {
+          console.log(3 + ' b error');
+          // next(error);
+          console.error(`stderr: ${data}`);
+          resolve();
+        });
+
+        gitRmClone.on('close', (data) => {
+          resolve();
+        });
+      }
+    })
+  });
+
+};
+
 // gitFetch = (branchName) => {
 //   const changeDir = spawn('cd', ['local_repo']);
 //   const gitCheckout = spawn('git', ['checkout', branchName]);
@@ -66,7 +74,7 @@ let mainBranch;
 //   });
 //
 //   gitFetch.stderr.on('data', (data) => {
-//     next(error);
+//     // next(error);
 //     console.error(`stderr: ${data}`);
 //   });
 // };
@@ -82,10 +90,6 @@ const api = axios.create({
   })
 });
 
-
-
-
-
 // Функции промежуточной обработки (middleware)
 app.use(express.json());
 app.use(express.urlencoded());
@@ -95,7 +99,6 @@ app.use(function(err, req, res, next) {
 });
 
 // Получение сохраненных настроек репозитория
-
 app.get('/api/settings', (req, res, next) => {
   api.get('/conf')
     .then((response) => {
@@ -108,6 +111,9 @@ app.get('/api/settings', (req, res, next) => {
 
 // Сохранение (Добавление или обновление) настроек репозитория
 // При это происходит клонирование репозитория на локальную машину
+// При повторном вызове - папка со старым репозиторием удаляется
+// и создается такая же папка, но уже с новым репозиторием
+// При этом возникает ошибка - надо разбираться
 app.post('/api/settings', (req, res, next) => {
   api.post('/conf', {
     "repoName": req.body.repoName,
@@ -116,8 +122,11 @@ app.post('/api/settings', (req, res, next) => {
     "period": +req.body.period
   })
     .then((response) => {
-      // gitClone();
-      res.json('Settings have been updated');
+      console.log(1);
+      cloneRepo(req.body.repoName).then(() => {
+        console.log(4);
+        res.json('Settings have been updated');
+      });
     })
     .catch((error) => {
       next(error);
@@ -136,7 +145,7 @@ app.get('/api/builds', (req, res, next) => {
 });
 
 // Добавление сборки в очередь для конкретного коммита
-// По полному хэшу коммита определяется полное сообщение, автор. Ветка берется по умолчанию
+// По полному хэшу коммита определяется полное сообщение, автор. Ветка пока берется по умолчанию
 app.post('/api/builds/:commitHash', (req, res, next) => {
   const log = spawn(`git show -s --format='%s===%an' ${req.params.commitHash}`, {shell: true});
 
@@ -178,7 +187,12 @@ app.get('/api/builds/:buildId', (req, res, next) => {
 app.get('/api/builds/:buildId/logs', (req, res, next) => {
   api.get('/build/log?buildId=' + req.params.buildId)
     .then((response) => {
-      res.send(response.data);
+      if (!response.data) {
+        res.send(String('Лога для этой сборки нет'))
+      }
+      else {
+        res.send(response.data);
+      }
     })
     .catch((error) => {
       next(error);
@@ -186,12 +200,32 @@ app.get('/api/builds/:buildId/logs', (req, res, next) => {
 });
 
 // Тестовая ручка для обновления локального репозитория (подтягивание последних изменений)
+// Пока не работает
 app.get('/api/test', (req, res, next) => {
   api.get('/conf')
     .then((response) => {
-      console.log(response.data);
-      // gitFetch(response.data.mainBranch);
-      res.send(response.data);
+      const updateRepo = spawn(`cd ${localRepoName} && git checkout ${response.data.data.mainBranch} && git pull`,{shell: true})
+      // const changeDir = spawn('cd', ['local_repo']);
+      // const gitCheckout = spawn('git', ['checkout', branchName]);
+      // const gitFetch = spawn('git', ['pull']);
+      //
+      // changeDir.stdout.pipe(gitCheckout.stdin);
+      // gitCheckout.stdout.pipe(gitFetch.stdin);
+
+      updateRepo.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        // res.send(`Репозиторий ${response.data.data.repoName} обновлен`);
+      });
+
+      updateRepo.stderr.on('data', (data) => {
+        // next(error);
+        console.error(`stderr: ${data}`);
+      });
+
+      updateRepo.on('close', (data) => {
+        console.log('child process exited with data ' + data);
+        res.send(String(`Репозиторий ${response.data.data.repoName} обновлен`));
+      });
     })
     .catch((error) => {
       next(error);
