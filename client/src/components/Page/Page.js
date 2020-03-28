7b165fcdbed9225e4bbd2f4c714c02c6345ce4cd
Dmitry Rakov <rakov.di@gmail.com>
2020-03-28 21:37:16 +0300

    [+] Переносит блок Main в реакт

--- client/src/components/Page/Page.js
+++ client/src/components/Page/Page.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 
-import { Router, Route, Switch } from 'react-router-dom';
-import history from '../../history';
-
 import Header from '../Header/Header';
-import StartScreen from '../../pages/StartScreen';
-import Settings from '../../pages/Settings';
-import BuildHistory from '../../pages/BuildHistory';
-import BuildDetails from '../../pages/BuildDetails';
+import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 
 class Page extends Component {
@@ -15,20 +9,7 @@ class Page extends Component {
     return (
       <div className="page">
         <Header />
-        {/*TODO Переписать по нормальному*/}
-        <main className={`main ${window.location.pathname === '/start-screen' && 'main_center'}`}>
-          <div className="main__container">
-            <Router history={history}>
-              <Switch>
-                <Route exact path='/' component={StartScreen} />
-                <Route path='/start-screen' component={StartScreen} />
-                <Route path='/settings' component={Settings} />
-                <Route path='/build-history' component={BuildHistory} />
-                <Route path='/build-details' component={BuildDetails} />
-              </Switch>
-            </Router>
-          </div>
-        </main>
+        <Main />
         <Footer />
       </div>
     );
