9ca1cbd466af6f48065338c9c6500d3907cafe56
Dmitry Rakov <rakov.di@gmail.com>
2020-04-15 05:17:26 +0300

    [+] Добавляет тесты для страницы settings

--- tests/integration-tests/Settings.test.js
+++ tests/integration-tests/Settings.test.js
@@ -1,13 +1,216 @@
+// require('dotenv').config();
 var assert = require('assert');
+// const { api, axiosAPI } = require('../../server/externalAPI/api');
 
-describe('Page Settings', function() {
-  it('Does page Settings exist - should find id', function() {
+const testData = {
+  repoName: 'rakov-di/homework_async',
+  repoNameIncorrect: 'rakov-di/homework_async123456789',
+  buildCommand: 'npm run build_prod',
+  mainBranch: 'master',
+  period: '100'
+};
+
+describe('Страница settings', function() {
+  beforeEach(async () => {
+    // Почему не работает???
+    // await api.deleteSettings();
+    // await axiosAPI.delete('/conf')
+    // await this.browser.pause(3000);
+    // (async () => await api.deleteSettings())();
+    // (async () => await axiosAPI.delete('/conf'))();
+    // (async () => await this.browser.pause(3000))();
+  });
+
+  it('Страница открывается', function() {
     return this.browser
-      .url('http://localhost:3000/settings')
-      .pause(3000)
-      .isExisting('.page_settings')
+      .url('/settings')
+      .waitForExist('.page_settings')
       .then(function(exist) {
-        assert.ok(exist, 'Settings page successfully opened')
+        assert.ok(exist, 'Страницы не загрузилась')
       });
+  });
+
+  it('По клику на кнопке "Cancel" происходит переход к странице "build-history"', function() {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .click('.form .btn-big_action_secondary')
+      .waitForExist('.page_build-history')
+      .then((exists) => {
+        assert.ok(exists, 'Переход к настройкам не произошел');
+      })
+  });
+
+  it('При заполнении поля repo появляется иконка "Очистить поле"', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .click('#repo')
+      .isExisting('.form__field_name_repo .icon_clear')
+      .then((exists) => {
+        assert.ok(exists, 'Иконка "Очистить поле" не появилась');
+      })
+  });
+
+  it('При заполнении поля command появляется иконка "Очистить поле"', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .click('#command')
+      .isExisting('.form__field_name_command .icon_clear')
+      .then((exists) => {
+        assert.ok(exists, 'Иконка "Очистить поле" не появилась');
+      })
+  });
+
+  it('При заполнении поля branch появляется иконка "Очистить поле"', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .click('#branch')
+      .isExisting('.form__field_name_branch .icon_clear')
+      .then((exists) => {
+        assert.ok(exists, 'Иконка "Очистить поле" не появилась');
+      })
+  });
+
+  // TODO Подумать, как обойтись без хака с написанием/стиранием одного символа
+  it('По клику на кнопку Save при незаполненном поле "repo" - оно подсвечивается красным, выдается ошибка', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .clearElement('#repo', '')
+      .click('#repo')
+      .keys(['f', '\u0008'])
+      .click('#command')
+      .keys([testData.buildCommand])
+      .click('#branch')
+      .keys([testData.mainBranch])
+      .click('#minutes')
+      .keys([testData.period])
+      .click('.form .btn-big_action_primary')
+      .waitForExist('.form__field_name_repo .input_invalid')
+      .isExisting('.form__field_name_repo .input_invalid')
+      .waitForExist('.form__field_name_repo .input__error-msg')
+      .isExisting('.form__field_name_repo .input__error-msg')
+      .then((exists) => {
+        assert.ok(exists, 'В пустом поле "repo" НЕ зафиксирована ошибка');
+      })
+  });
+
+  it('По клику на кнопку Save при незаполненном поле "command" - оно подсвечивается красным, выдается ошибка', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .click('#repo')
+      .keys([testData.repoName])
+      .clearElement('#command')
+      .click('#command')
+      .keys(['f', '\u0008'])
+      .click('#branch')
+      .keys([testData.mainBranch])
+      .click('#minutes')
+      .keys([testData.period])
+      .click('.form .btn-big_action_primary')
+      .waitForExist('.form__field_name_command .input_invalid')
+      .isExisting('.form__field_name_command .input_invalid')
+      .waitForExist('.form__field_name_command .input__error-msg')
+      .isExisting('.form__field_name_command .input__error-msg')
+      .then((exists) => {
+        assert.ok(exists, 'В пустом поле "command" НЕ зафиксирована ошибка');
+      })
+  });
+
+  //TODO Посмотреть можно ли написать нормальную маску для НЕ цифр (regExp??)
+  it('При попытке ввода в поле "minutes" НЕ числа - оно остается пустым', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .clearElement('#minutes')
+      .click('#minutes')
+      .keys(['a.,:;/-=_!@#$%^&*()_+'])
+      .getValue('#minutes')
+      .then(function(value) {
+        assert.equal(value, '', 'В поле можно вводить что-то кроме цифр')
+      });
+  });
+
+  // ?? Возможно, стоит разделить disable/enable на 2 теста
+  it('По клику на кнопку Save при всех заполненных полях - кнопки должны дизейблиться, при завершении запроса - энейблиться', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .clearElement('#repo')
+      .click('#repo')
+      .keys([testData.repoName])
+      .clearElement('#command')
+      .click('#command')
+      .keys([testData.buildCommand])
+      .clearElement('#branch')
+      .click('#branch')
+      .keys([testData.mainBranch])
+      .clearElement('#minutes')
+      .click('#minutes')
+      .keys([testData.period])
+      .click('.form .btn-big_action_primary')
+      .waitForExist('.form .btn-big_action_primary[disabled]')
+      .isExisting('.form .btn-big_action_primary[disabled]')
+      .waitForExist('.form .btn-big_action_secondary[disabled]')
+      .isExisting('.form .btn-big_action_secondary[disabled]')
+      .waitForEnabled('.form .btn-big_action_primary')
+      .isEnabled('.form .btn-big_action_primary')
+      .waitForEnabled('.form .btn-big_action_secondary')
+      .isEnabled('.form .btn-big_action_secondary')
+      .then((exists) => {
+        assert.ok(exists, 'Кнопки НЕ в состоянии disabled');
+      })
+  });
+
+  it('При сохранении корректных настроек - выдается сообщение об успешном сохранении', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .clearElement('#repo')
+      .click('#repo')
+      .keys([testData.repoName])
+      .clearElement('#command')
+      .click('#command')
+      .keys([testData.buildCommand])
+      .clearElement('#branch')
+      .click('#branch')
+      .keys([testData.mainBranch])
+      .clearElement('#minutes')
+      .click('#minutes')
+      .keys([testData.period])
+      .click('.form .btn-big_action_primary')
+      .waitForExist('.form__success-msg')
+      .isExisting('.form__success-msg')
+      .then((exists) => {
+        assert.ok(exists, 'Сообщение об успешном сохранении настроек не показано');
+      })
+  });
+
+  it('При сохранении НЕкорректных настроек - выдается сообщение о невозможности сохранения', function () {
+    return this.browser
+      .url('/settings')
+      .waitForExist('.page_settings')
+      .clearElement('#repo')
+      .click('#repo')
+      .keys([testData.repoNameIncorrect])
+      .clearElement('#command')
+      .click('#command')
+      .keys([testData.buildCommand])
+      .clearElement('#branch')
+      .click('#branch')
+      .keys([testData.mainBranch])
+      .clearElement('#minutes')
+      .click('#minutes')
+      .keys([testData.period])
+      .click('.form .btn-big_action_primary')
+      .waitForExist('.form__error-msg')
+      .isExisting('.form__error-msg')
+      .then((exists) => {
+        assert.ok(exists, 'Сообщение о невозможности сохранения настроек не показано');
+      })
   });
 });
