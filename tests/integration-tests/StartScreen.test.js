ae8ad66178c2753c9dd8491c960178aa32704817
Dmitry Rakov <rakov.di@gmail.com>
2020-04-15 12:32:24 +0300

    [+] Добавляет возможность очищать репозиторий перед запуском тестов

--- tests/integration-tests/StartScreen.test.js
+++ tests/integration-tests/StartScreen.test.js
@@ -1,22 +1,13 @@
-require('dotenv').config();
+// похоже на костыль, но пока так
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, '../../server/.env')});
+
 const assert = require('assert');
-
-const { api, axiosAPI } = require('../../server/externalAPI/api');
-
-const MockAdapter = require('axios-mock-adapter');
-const mock = new MockAdapter(axiosAPI);
-mock.onGet('/conf').reply(200, {});
-
-api.deleteSettings();
+const { api } = require('../../server/externalAPI/api');
 
 describe('Страница start-screen', function() {
   beforeEach(async () => {
-    api.deleteSettings();
-
-    await axiosAPI.delete(`/conf`);
-
-    // (async () => await api.deleteSettings())();
-    // (async () => await this.browser.pause(1000))();
+    await api.deleteSettings();
   });
 
   it('Страница открывается', function() {
@@ -39,16 +30,16 @@ describe('Страница start-screen', function() {
       })
   });
 
-it('По клику на кнопке "Settings" в Header происходит переход к странице "Settings"', function() {
-  return this.browser
-    .url('start-screen')
-    .waitForExist('.page_start-screen')
-    .click('.header .icon_settings-before')
-    .waitForExist('.page_settings')
-    .then((exists) => {
-      assert.ok(exists, 'Переход к настройкам не произошел');
-    })
-});
+  it('По клику на кнопке "Settings" в Header происходит переход к странице "Settings"', function() {
+    return this.browser
+      .url('start-screen')
+      .waitForExist('.page_start-screen')
+      .click('.header .icon_settings-before')
+      .waitForExist('.page_settings')
+      .then((exists) => {
+        assert.ok(exists, 'Переход к настройкам не произошел');
+      })
+  });
 });
 
 
