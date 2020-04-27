c71e5baafbd0fd161014f7a28c58fd47b2d48957
Dmitry Rakov <rakov.di@gmail.com>
2020-04-27 21:04:14 +0300

    [*] Избавляется от any в actions

--- client/src/redux/actions/actions.ts
+++ client/src/redux/actions/actions.ts
@@ -7,29 +7,42 @@ import Convert from 'ansi-to-html';
 
 const convert = new Convert({fg: '#000', bg: '#000'});
 
-export type ActionsTypes = any;
+type Response = {
+  data: {
+    payload: any;
+    message: string;
+  }
+}
+
+type Error = {
+  response: {
+    data: {
+      message: string
+    }
+  }
+}
 
 export const getCurSettings = () => {
   return (dispatch: Dispatch<any>) => {
     dispatch(fetchStart());
 
     api.getSettings()
-      .then((res: any) => {
+      .then((res: Response) => {
         dispatch(updateStoreSettings(res.data.payload));
         dispatch(fetchDone());
       })
-      .catch((err: any) => {
+      .catch((err: Error) => {
         dispatch(fetchFail(err.response && err.response.data && err.response.data.message));
       });
   };
 };
 
 export const updateSettings = (settings: UpdateSettingsParams) => {
-  return (dispatch: Dispatch<any>) => {
+  return (dispatch: Dispatch<ActionTypes>) => {
     dispatch(fetchStart());
 
     api.updateSettings(settings)
-      .then((res: any) => {
+      .then((res: Response) => {
         dispatch(updateStoreSettings(settings));
         dispatch(updateFormStatus({
           value: 'success',
@@ -37,7 +50,7 @@ export const updateSettings = (settings: UpdateSettingsParams) => {
         }));
         dispatch(fetchDone());
       })
-      .catch((err: any) => {
+      .catch((err: Error) => {
         dispatch(updateFormStatus({
           value: 'error',
           text: err.response.data.message
@@ -47,37 +60,37 @@ export const updateSettings = (settings: UpdateSettingsParams) => {
   };
 };
 
-export const getBuildsList = () => {
-  return (dispatch: Dispatch<any>) => {
+export const getBuildsList = ():any => {
+  return (dispatch: Dispatch<ActionTypes>) => {
     dispatch(fetchStart());
 
     api.getBuildsList()
-      .then((res: any) => {
+      .then((res: Response) => {
         dispatch(updateStoreBuildsList(res.data.payload));
         dispatch(fetchDone());
       })
-      .catch((err: any) => {
+      .catch((err: Error) => {
         dispatch(updateFormStatus({
           value: 'error',
-          text: err.message
+          text: err.response.data.message
         }));
-        dispatch(fetchFail(err.response && err.response.data && err.response.data.message));
+        dispatch(fetchFail(err.response.data.message));
       });
   };
 };
 
 export const addCommitToQueue = (commitHash: string) => {
-  return (dispatch: Dispatch<any>) => {
+  return (dispatch: Dispatch<ActionTypes>) => {
     dispatch(fetchStart());
 
     api.addCommitToQueue(commitHash)
-      .then((res: any) => {
+      .then((res: Response) => {
         dispatch(getBuildsList()); // ???
         document.location.href = `/build/${res.data.payload.id}`;
         // dispatch(updateStoreBuildsList(res.data.data));
         // dispatch(fetchDone());
       })
-      .catch((err: any) => {
+      .catch((err: Error) => {
         dispatch(inputSetValidationStatus('commitHash', false))
         dispatch(fetchFail(err.response && err.response.data && err.response.data.message));
       });
@@ -85,66 +98,77 @@ export const addCommitToQueue = (commitHash: string) => {
 };
 
 export const getBuildDetails = (buildId: string) => {
-  return (dispatch: Dispatch<any>) => {
+  return (dispatch: Dispatch<ActionTypes>) => {
     dispatch(fetchStart());
 
     api.getBuildDetails(buildId)
-      .then((res: any) => {
+      .then((res: Response) => {
         dispatch(updateStoreCurBuildDetails(res.data.payload));
         dispatch(fetchDone());
       })
-      .catch((err: any) => {
-        dispatch(fetchFail(err.response && err.response.data && err.response.data.message));
+      .catch((err: Error) => {
+        dispatch(fetchFail(err.response.data.message));
       });
   };
 };
 
 export const getBuildLog = (buildId: string) => {
-  return (dispatch: Dispatch<any>) => {
+  return (dispatch: Dispatch<ActionTypes>) => {
     dispatch(fetchStart());
 
     api.getBuildLog(buildId)
-      .then((res: any) => {
+      .then((res: Response) => {
         dispatch(updateStoreCurBuildLog(convert.toHtml(res.data.payload)));
         dispatch(fetchDone());
       })
-      .catch((err: any) => {
-        dispatch(fetchFail(err.response && err.response.data && err.response.data.message));
+      .catch((err: Error) => {
+        dispatch(fetchFail(err.response.data.message));
       });
   };
 };
 
-export const fetchStart = () => ({
+type ActionType = {
+  type: string;
+}
+
+type ActionTypeAndPayload = {
+  type: string;
+  payload: any
+}
+
+type ActionTypes = ActionType | ActionTypeAndPayload
+
+export const fetchStart = ():ActionType => ({
   type: ACTIONS.FETCH_START,
 });
 
-export const fetchDone = () => ({
+export const fetchDone = ():ActionType => ({
   type: ACTIONS.FETCH_DONE
 });
 
-export const fetchFail = (err: any) => ({
+export const fetchFail = (err: string):ActionTypeAndPayload => ({
   type: ACTIONS.FETCH_FAIL,
   payload: {
     err: err
   }
 });
 
-export const updateStoreSettings = (payload: UpdateSettingsParams) => ({
+export const updateStoreSettings = (payload: UpdateSettingsParams):ActionTypeAndPayload => ({
   type: ACTIONS.UPDATE_STORE_SETTINGS,
   payload
 });
 
-export const updateStoreBuildsList = (payload: any) => ({
+export const updateStoreBuildsList = (payload: any):ActionTypeAndPayload => ({
   type: ACTIONS.UPDATE_STORE_BUILDS_LIST,
   payload
 });
 
-export const updateStoreCurBuildDetails = (payload: any) => ({
+export const updateStoreCurBuildDetails = (payload: any):ActionTypeAndPayload => ({
   type: ACTIONS.UPDATE_STORE_CUR_BUILD_DETAILS,
   payload
 });
 
-export const updateStoreCurBuildLog = (payload: string) => ({
+export const updateStoreCurBuildLog = (payload: string):ActionTypeAndPayload => ({
   type: ACTIONS.UPDATE_STORE_CUR_BUILD_LOG,
   payload
 });
@@ -154,35 +178,55 @@ type FormStatus = {
   text: string;
 }
 
-export const updateFormStatus = (payload: FormStatus) => {
+export const updateFormStatus = (payload: FormStatus):ActionTypeAndPayload => {
   return {
     type: ACTIONS.UPDATE_FORM_STATUS,
     payload
   }
 };
 
+// TODO Кажется, можно их как-то унифицировать до одного типа
+type ActionModalVisibilityToggle = {
+  type: string;
+  status: boolean;
+}
+
+type ActionInputSetValue = {
+  type: string;
+  name: string;
+  value: string;
+}
+
+type ActionInputSetValidationStatus = {
+  type: string;
+  name: string;
+  status: boolean;
+}
+
+type ActionsModal = ActionModalVisibilityToggle | ActionInputSetValue | ActionInputSetValidationStatus
+
 export const openModal = () => {
-  return (dispatch: Dispatch<any>) => {
+  return (dispatch: Dispatch<ActionsModal>) => {
     dispatch(modalVisibilityToggle(true));
   };
 };
 
 export const closeModal = () => {
-  return (dispatch: Dispatch<any>) => {
+  return (dispatch: Dispatch<ActionsModal>) => {
     dispatch(modalVisibilityToggle(false));
     dispatch(inputSetValue('commitHash', ''));
     dispatch(inputSetValidationStatus('commitHash', true));
   };
 };
 
-export const modalVisibilityToggle = (status: boolean) => {
+export const modalVisibilityToggle = (status: boolean):ActionModalVisibilityToggle => {
   return {
     type: ACTIONS.MODAL_VISIBILITY_TOGGLE,
     status
   }
 };
 
-export const inputSetValue = (name: string, value: string) => {
+export const inputSetValue = (name: string, value: string):ActionInputSetValue => {
   return {
     type: ACTIONS.INPUT_SET_VALUE,
     name,
@@ -190,7 +234,7 @@ export const inputSetValue = (name: string, value: string) => {
   };
 };
 
-export const inputSetValidationStatus = (name: string, status: boolean) => {
+export const inputSetValidationStatus = (name: string, status: boolean):ActionInputSetValidationStatus => {
   return {
     type: ACTIONS.INPUT_SET_VALIDATION_STATUS,
     name,
